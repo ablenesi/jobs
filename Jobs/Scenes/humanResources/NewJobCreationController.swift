35534576f6bd615075318f0f685f47b28fe0db29
Maier Levente <levente.maier@gmail.com>
2018-01-13 12:38:52 -0800

    + SaveNewJobButton

--- Jobs/Scenes/humanResources/NewJobCreationController.swift
+++ Jobs/Scenes/humanResources/NewJobCreationController.swift
@@ -10,14 +10,20 @@ import UIKit
 
 class NewJobCreationController: UIViewController {
 
+    @IBOutlet weak var jobTitleTextField: UITextField!
+    
     @IBOutlet weak var jobDescriptionTextView: UITextView!
+    
+    @IBOutlet weak var saveNewJobButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         jobDescriptionTextView.layer.borderWidth = 0.5
         jobDescriptionTextView.layer.borderColor = UIColor.lightGray.cgColor
-
+        
+        saveNewJobButton.backgroundColor = UIColor.blue
+        saveNewJobButton.setTitleColor(.white, for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +31,9 @@ class NewJobCreationController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveNewJobAction(_ sender: Any) {
     }
-    */
+    
+    
 
 }
