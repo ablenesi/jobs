52ea0072f4fe1551fe5e6ae914a86700e0277492
Maier Levente <levente.maier@gmail.com>
2018-01-14 15:34:57 -0800

    Added Milestones on CandidateProgress Screen

--- Jobs/Scenes/humanResources/CandidateProgressController.swift
+++ Jobs/Scenes/humanResources/CandidateProgressController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class CandidateProgressController: UIViewController {
+class CandidateProgressMilestoneViewCell: UITableViewCell {
+    
+    @IBOutlet weak var milestoneStateImageView: UIImageView!
+    @IBOutlet weak var milestoneNameLabel: UILabel!
+}
+
+class CandidateProgressController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var candidateNameLabel: UILabel!
+    
+    @IBOutlet weak var candidateMilestoneTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         candidateNameLabel.text = "Steven Schwenke"
+        
+        candidateMilestoneTableView.delegate = self
+        candidateMilestoneTableView.dataSource = self
+        
+        candidateMilestoneTableView.rowHeight = 125
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = candidateMilestoneTableView.dequeueReusableCell(withIdentifier: "candidateProgressMilestoneId", for: indexPath) as!CandidateProgressMilestoneViewCell
+        
+        cell.milestoneNameLabel?.text = "HR Interview"
+        
+        return cell
+        
+    }
 }
